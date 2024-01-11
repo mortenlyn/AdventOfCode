3a1cafc6fc554e7917cd91d66496ede8793ad711
Morten Madsen Lyngstad <morteml@stud.ntnu.no>
2024-01-11 21:59:59 +0100

    Day 4 Finished

---
+++ adventofcode2015python/day4/main.py
@@ -0,0 +1,16 @@
+import hashlib
+
+
+def part1():
+    data = open("adventofcode2015python\day4\input.txt", "r").read().strip()
+
+    counter = 0
+    while True:
+        counter += 1
+        hash_object = hashlib.md5((data + str(counter)).encode())
+        if hash_object.hexdigest().startswith("000000"):
+            print(counter)
+            break
+
+
+part1()
