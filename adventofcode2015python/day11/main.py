5008c74c361f0855544352960b0d75a3480203fc
Morten M Lyngstad <Morteml@stud.ntnu.no>
2024-02-20 13:35:10 +0100

    Improved and generalized day 11

--- adventofcode2015python/day11/main.py
+++ adventofcode2015python/day11/main.py
@@ -1,39 +1,30 @@
-from collections import Counter
+import re
+import string
 
 
 def first_req(data):
-    for i in range(len(data) - 2):
-        current_letter = ord(data[i])
-        next_letter = ord(data[i+1])
-        second_next_letter = ord(data[i+2])
-        if (next_letter == (current_letter + 1)) and (second_next_letter == (next_letter + 1)):
-            return True
+    return any(tripple in data for tripple in (string.ascii_lowercase[i:i+3] for i in range(len(string.ascii_lowercase) - 2)))
 
-    return False
+    # for i in range(len(data) - 2):
+    #     current_letter = ord(data[i])
+    #     next_letter = ord(data[i+1])
+    #     second_next_letter = ord(data[i+2])
+    #     if (next_letter == (current_letter + 1)) and (second_next_letter == (next_letter + 1)):
+    #         return True
+
+    # return False
 
 
 def second_req(data):
-    for let in ["i", "o", "l"]:
-        if let in data:
-            return False
-    return True
+    return not any(let in data for let in "iol")
 
 
 def third_req(data):
-    counter = 0
-    index = 0
-
-    while index < (len(data)-1):
-        if data[index] == data[index + 1]:
-            counter += 1
-            index += 1
-        index += 1
-
-    return counter >= 2
+    return len(re.findall(r".*(.)\1.*(.)\2.*", data)) != 0
 
 
 def part1():
-    data = ([*"hxbxwxba"])
+    data = [*"hxbxwxba"]
 
     reversed_data = list(reversed(data))
 
@@ -49,16 +40,20 @@ def part1():
 
     return "".join(data)
 
-
-print("Part 1:", part1())
+first_string = part1()
+print("Part 1:", first_string)
 
 
 def part2():
-    data = ([*"hxbxxzaa"])
+    data = [*first_string]
 
     reversed_data = list(reversed(data))
-
-    while not (first_req("".join(data)) and second_req("".join(data)) and third_req("".join(data))):
+    first = True
+    while True:
+        if (first_req("".join(data)) and second_req("".join(data)) and third_req("".join(data))):
+            if not first:
+                return "".join(data)
+            first = not first
         reversed_data[0] = chr(ord(reversed_data[0]) + 1)
 
         for i in range(len(reversed_data)):
@@ -67,8 +62,6 @@ def part2():
                 reversed_data[i+1] = chr(ord(reversed_data[i+1]) + 1)
 
         data = list(reversed(reversed_data))
-
-    return "".join(data)
 
 
 print("Part 2:", part2())
