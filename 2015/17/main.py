df0a80a0f456c0a848f357326b1311def4cf32f4
Morten M Lyngstad <Morteml@stud.ntnu.no>
2024-03-29 13:48:40 +0100

    Day 17 Finished

--- 2015/17/main.py
+++ 2015/17/main.py
@@ -1,15 +1,7 @@
-import functools
-import itertools
-import os
-import re
-import string
 import sys
 import time
-from collections import defaultdict, deque
-from pprint import pprint
 
 sys.path.insert(0, "../../")
-from utils import copy_answer, request_submit, write_solution
 
 
 def parseLine(line):
@@ -20,24 +12,43 @@ class Solution:
     def __init__(self, test=False):
         self.test = test
         filename = "testinput.txt" if self.test else "input.txt"
-        self.data = [parseLine(line) for line in open(filename).read().rstrip().split("\n")]
+        self.data = [
+            parseLine(line) for line in open(filename).read().rstrip().split("\n")
+        ]
 
     def part1(self):
-        return None
+        container_sizes = sorted(list(map(int, self.data)))
+
+        def find_combinations(numbers, target):
+            if target == 0:
+                return 1
+            if not numbers or target < 0:
+                return 0
+            return find_combinations(numbers[1:], target) + find_combinations(
+                numbers[1:], target - numbers[0]
+            )
+
+        return find_combinations(container_sizes, 150)
 
     def part2(self):
-        return None
+        container_sizes = sorted(list(map(int, self.data)))
+
+        def find_min_subset(num_of_containers, target_capacity):
+            smallest_subset_size = target_capacity
+            for index, container_size in enumerate(container_sizes[:num_of_containers]):
+                if container_size <= target_capacity:
+                    subset_size = (
+                        find_min_subset(index, target_capacity - container_size) + 1
+                    )
+                    if subset_size < smallest_subset_size:
+                        smallest_subset_size = subset_size
+            return smallest_subset_size
+
+        return find_min_subset(len(container_sizes), 150)
 
 
 def main():
     start = time.perf_counter()
-
-    test = Solution(test=True)
-    test1 = test.part1()
-    test2 = test.part2()
-    print(f"(TEST) Part 1: {test1}, \t{'correct :)' if test1 == None else 'wrong :('}")
-    print(f"(TEST) Part 2: {test2}, \t{'correct :)' if test2 == None else 'wrong :('}")
-
     solution = Solution()
     part1 = solution.part1()
     part2 = solution.part2()
@@ -45,10 +56,6 @@ def main():
     print(part2_text := f"Part 2: {part2}")
 
     print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
-
-    copy_answer(part1, part2)
-    write_solution(os.path.dirname(os.path.realpath(__file__)), part1_text, part2_text)
-    request_submit(2015, 17, part1, part2)
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -14,9 +14,9 @@ This repository contains my solutions to the challenges from Advent of Code.
 [2018]  0*
 [2017] 16*
 [2016] 16*
-[2015] 32*
+[2015] 34*
 
-Total stars: 108*
+Total stars: 110*
 ```
 <!-- END STATS -->
 
