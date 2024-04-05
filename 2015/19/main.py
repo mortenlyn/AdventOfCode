a0992679e8787a8e7c331c9786fb47414c9a993e
Morten Madsen Lyngstad <morteml@stud.ntnu.no>
2024-04-05 17:00:28 +0200

    Finished day 19

--- 2015/19/main.py
+++ 2015/19/main.py
@@ -1,8 +1,4 @@
-import functools
-import itertools
-import os
 import re
-import string
 import sys
 import time
 import heapq
@@ -10,7 +6,6 @@ from collections import defaultdict, deque
 from pprint import pprint
 
 sys.path.insert(0, "../../")
-from utils import copy_answer, request_submit, write_solution
 
 
 def parseLine(line):
@@ -73,12 +68,6 @@ class Solution:
 def main():
     start = time.perf_counter()
 
-    test = Solution(test=True)
-    test1 = test.part1()
-    test2 = test.part2()
-    print(f"(TEST) Part 1: {test1}, \t{'correct :)' if test1 == 4 else 'wrong :('}")
-    print(f"(TEST) Part 2: {test2}, \t{'correct :)' if test2 == None else 'wrong :('}")
-
     solution = Solution()
     part1 = solution.part1()
     part2 = solution.part2()
@@ -86,10 +75,6 @@ def main():
     print(part2_text := f"Part 2: {part2}")
 
     print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
-
-    copy_answer(part1, part2)
-    write_solution(os.path.dirname(os.path.realpath(__file__)), part1_text, part2_text)
-    request_submit(2015, 19, None, part2)
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -14,9 +14,9 @@ This repository contains my solutions to the challenges from Advent of Code.
 [2018]  0*
 [2017] 16*
 [2016] 16*
-[2015] 37*
+[2015] 38*
 
-Total stars: 113*
+Total stars: 114*
 ```
 <!-- END STATS -->
 
