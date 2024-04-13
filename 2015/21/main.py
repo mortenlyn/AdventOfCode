b81f49d3683f2bfe353a4ae3703de4f6175b92f7
Morten Madsen Lyngstad <morteml@stud.ntnu.no>
2024-04-13 17:56:26 +0200

    Day 21 Finished

--- 2015/21/main.py
+++ 2015/21/main.py
@@ -1,15 +1,8 @@
-import functools
 import itertools
-import os
-import re
-import string
 import sys
 import time
-from collections import defaultdict, deque
-from pprint import pprint
 
 sys.path.insert(0, "../../")
-from utils import copy_answer, request_submit, write_solution
 
 
 def parseLine(line):
@@ -121,10 +114,6 @@ def main():
     print(part2_text := f"Part 2: {part2}")
 
     print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
-
-    copy_answer(part1, part2)
-    write_solution(os.path.dirname(os.path.realpath(__file__)), part1_text, part2_text)
-    request_submit(2015, 21, None, part2)
 
 
 if __name__ == "__main__":
