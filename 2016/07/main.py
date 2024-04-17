3bdaf089daaaf81e9f8667f1c9c841a8258a90e9
Morten Madsen Lyngstad <morteml@stud.ntnu.no>
2024-04-17 21:44:14 +0200

    Day 7 Part 1 Finished

---
+++ 2016/07/main.py
@@ -0,0 +1,122 @@
+import functools
+import itertools
+import os
+import re
+import string
+import sys
+import time
+from collections import defaultdict, deque
+from pprint import pprint
+
+sys.path.insert(0, "../../")
+from utils import copy_answer, request_submit, write_solution
+
+
+def parseLine(line):
+    return line
+
+
+class Solution:
+    def __init__(self, test=False):
+        self.test = test
+        filename = "testinput.txt" if self.test else "input.txt"
+        self.data = [
+            parseLine(line) for line in open(filename).read().rstrip().split("\n")
+        ]
+
+    def supportsTLS(self, sequence):
+        reverse_pair = re.findall(r"([a-zA-Z])([a-zA-Z])\2\1", sequence)
+        for pair in reverse_pair:
+            if pair[0] == pair[1]:
+                return False
+        if reverse_pair:
+            return True
+
+    def part1(self):
+        result = 0
+
+        sequences = [re.split(r"\[|\]", line) for line in self.data]
+
+        for line in sequences:
+            supports_TLS = False
+
+            for i, sequence in enumerate(line):
+                if i % 2 == 1:
+                    if self.supportsTLS(sequence):
+                        supports_TLS = False
+                        break
+                if i % 2 == 0:
+                    if self.supportsTLS(sequence):
+                        supports_TLS = True
+
+            if supports_TLS:
+                result += 1
+
+        return result
+
+    def findABA(self, sequence):
+        aba = re.findall(r"(?=([a-zA-Z])([a-zA-Z])(\1))", sequence)
+
+        if not aba:
+            return None
+        for pair in aba:
+            if pair[0] == pair[1]:
+                return None
+
+        return aba
+
+    def findBAB(self, sequence, aba):
+        bab = re.findall(r"(?=([a-zA-Z])([a-zA-Z])(\1))", sequence)
+        wanted = aba[1] + aba[0] + aba[1]
+
+        for pair in bab:
+            if "".join(pair) == wanted:
+                return True
+        return False
+
+    def part2(self):
+        result = 0
+
+        sequences = [re.split(r"\[|\]", line) for line in self.data]
+
+        for line in sequences:
+            aba1 = self.findABA(line[0])
+            aba2 = self.findABA(line[2])
+
+            if aba1 or aba2:
+                if aba1:
+                    for aba in aba1:
+                        if self.findBAB(line[1], aba):
+                            result += 1
+
+                if aba2:
+                    for aba in aba2:
+                        if self.findBAB(line[1], aba):
+                            result += 1
+
+        return result
+
+
+def main():
+    start = time.perf_counter()
+
+    test = Solution(test=True)
+    test1 = test.part1()
+    test2 = test.part2()
+    print(f"(TEST) Part 1: {test1}, \t{'correct :)' if test1 == None else 'wrong :('}")
+    print(f"(TEST) Part 2: {test2}, \t{'correct :)' if test2 == None else 'wrong :('}")
+    solution = Solution()
+    part1 = solution.part1()
+    part2 = solution.part2()
+    print(part1_text := f"Part 1: {part1}")
+    print(part2_text := f"Part 2: {part2}")
+
+    print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
+
+    copy_answer(part1, part2)
+    write_solution(os.path.dirname(os.path.realpath(__file__)), part1_text, part2_text)
+    request_submit(2016, 7, part1, part2)
+
+
+if __name__ == "__main__":
+    main()
