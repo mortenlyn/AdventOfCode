88bda9f8caba29a0ee6d274fd8dfc687cc45c6ed
Morten Madsen Lyngstad <morteml@stud.ntnu.no>
2024-05-12 21:22:03 +0200

    Day 5 Finished

--- 2021/05/main.py
+++ 2021/05/main.py
@@ -1,15 +1,7 @@
-import functools
-import itertools
-import os
-import re
-import string
 import sys
 import time
-from collections import defaultdict, deque
-from pprint import pprint
 
 sys.path.insert(0, "../../")
-from utils import copy_answer, request_submit, write_solution
 
 
 def parseLine(line):
@@ -24,8 +16,6 @@ class Solution:
             parseLine(line) for line in open(filename).read().rstrip().split("\n")
         ]
 
-        self.ocean_floor = []
-
         self.max_x = 0
         self.max_y = 0
 
@@ -38,11 +28,16 @@ class Solution:
             self.max_x = max(self.max_x, int(x_start), int(x_end))
             self.max_y = max(self.max_y, int(y_start), int(y_end))
 
+    def create_ocean_floor(self):
+        ocean_floor = []
+
         for _ in range(self.max_y + 1):
-            self.ocean_floor.append([0] * (self.max_x + 1))
+            ocean_floor.append([0] * (self.max_x + 1))
+
+        return ocean_floor
 
     def part1(self):
-        ocean_floor = self.ocean_floor.copy()
+        ocean_floor = self.create_ocean_floor()
 
         for line in self.data:
             vent_coords_start = line.split(" -> ")[0]
@@ -71,18 +66,47 @@ class Solution:
         return count
 
     def part2(self):
-        pass
+        ocean_floor = self.create_ocean_floor()
+
+        for line in self.data:
+            vent_coords_start = line.split(" -> ")[0]
+            x_start, y_start = vent_coords_start.split(",")
+            vent_coords_end = line.split(" -> ")[1]
+            x_end, y_end = vent_coords_end.split(",")
+
+            if x_start == x_end:
+                for y in range(
+                    min(int(y_start), int(y_end)), max(int(y_start), int(y_end)) + 1
+                ):
+                    ocean_floor[y][int(x_start)] += 1
+
+            elif y_start == y_end:
+                for x in range(
+                    min(int(x_start), int(x_end)), max(int(x_start), int(x_end)) + 1
+                ):
+                    ocean_floor[int(y_start)][x] += 1
+            else:
+                slope = (int(y_end) - int(y_start)) / (int(x_end) - int(x_start))
+                intercept = int(y_start) - slope * int(x_start)
+
+                for x in range(
+                    min(int(x_start), int(x_end)), max(int(x_start), int(x_end)) + 1
+                ):
+                    y = slope * x + intercept
+                    ocean_floor[int(y)][x] += 1
+
+        count = 0
+        for x in range(self.max_y + 1):
+            for y in range(self.max_x + 1):
+                if ocean_floor[x][y] >= 2:
+                    count += 1
+
+        return count
 
 
 def main():
     start = time.perf_counter()
 
-    test = Solution(test=True)
-    test1 = test.part1()
-    test2 = test.part2()
-    print(f"(TEST) Part 1: {test1}, \t{'correct :)' if test1 == None else 'wrong :('}")
-    print(f"(TEST) Part 2: {test2}, \t{'correct :)' if test2 == None else 'wrong :('}")
-    # quit()
     solution = Solution()
     part1 = solution.part1()
     part2 = solution.part2()
@@ -90,10 +114,6 @@ def main():
     print(part2_text := f"Part 2: {part2}")
 
     print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
-
-    copy_answer(part1, part2)
-    write_solution(os.path.dirname(os.path.realpath(__file__)), part1_text, part2_text)
-    request_submit(2021, 5, part1, part2)
 
 
 if __name__ == "__main__":
